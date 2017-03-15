2cc713a16255af41b85102932a1d2a5e0e04e98c
Sitora <redastify@gmail.com>
2017-03-16 00:59:33 +0300

    Готов вывод всей информации о рецепте

--- ios/Chief/Chief/SecondViewController.swift
+++ ios/Chief/Chief/SecondViewController.swift
@@ -12,7 +12,8 @@ class SecondViewController:UIViewController, UITableViewDataSource, UITableViewD
   //  var tableView: UITableView!
     var hidden:Bool?=false
     @IBOutlet var stackView: UIStackView!
-    @IBOutlet var textLabel: UILabel!
+   // @IBOutlet var textLabel: UILabel!
+    @IBOutlet var textView: UITextView!
     @IBOutlet var ingredientsLabel: UILabel!
     @IBOutlet var contentView: UIView!
     @IBOutlet var scrollView: UIScrollView!
@@ -26,9 +27,11 @@ class SecondViewController:UIViewController, UITableViewDataSource, UITableViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         self.ingredientsLabel.numberOfLines=0;
-        self.textLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
-        self.textLabel.adjustsFontSizeToFitWidth = false;
+        self.automaticallyAdjustsScrollViewInsets = false
+//        self.textLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+//        self.textLabel.adjustsFontSizeToFitWidth = false;
         scrollView.contentSize = contentView.frame.size;
+        scrollView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func btnClicked(_ sender: Any) {
@@ -41,13 +44,22 @@ class SecondViewController:UIViewController, UITableViewDataSource, UITableViewD
         }
         self.nameLabel.text=self.recipe.name;
         self.nameLabel.sizeToFit()
-        self.textLabel.numberOfLines=0;
-        self.textLabel.text = self.recipe.text
+//        self.textLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+//        self.textLabel.numberOfLines=0;
+//        self.textLabel.text = self.recipe.text
         for ingredient in recipe.ingredients{
             self.ingredientsLabel.text=self.ingredientsLabel.text! + "\r\n"+ingredient.name
         }
         self.ingredientsLabel.sizeToFit()
-        self.textLabel.sizeToFit()
+        self.textView.isEditable=false
+        self.textView.text=recipe.text
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame;
+//        self.textLabel.sizeToFit()
     }
     @IBAction func hideShowButtonClicked(_ sender: UIButton) {
         if hidden!{
@@ -66,30 +78,9 @@ class SecondViewController:UIViewController, UITableViewDataSource, UITableViewD
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension;
         tableView.delegate = self
-        //stackView.addArrangedSubview(self.tableView)
-        //self.view.addSubview(self.tableView)
-        
-//        let btn = UIButton(frame: CGRect(x: 0, y: 25, width: self.view.frame.width, height: 20))
-//        btn.backgroundColor = UIColor.cyan
-//        btn.setTitle("Add new Dummy", for: UIControlState.normal)
-       // btn.addTarget(self, action: #selector(SecondViewController.addDummyData), for: UIControlEvents.touchUpInside)
-        //self.view.addSubview(btn)
-        //stackView.addSubview(btn)
         
         showHideButton.setTitleColor(UIColor.black, for: .normal)
     }
-    
-//    @IBAction func addDummyData() {
-//        RestApiManager.sharedInstance.getRandomUser { (json: JSON) in
-//            self.recipe = Recipe(json: json)
-//            DispatchQueue.main.async(execute: {
-//                self.tableView.reloadData()
-//                self.tableView.rowHeight = UITableViewAutomaticDimension;
-//            })
-//        }
-//        self.nameLabel.text=self.recipe.name;
-//        self.nameLabel.sizeToFit()
-//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.recipe.ingredients.count;
